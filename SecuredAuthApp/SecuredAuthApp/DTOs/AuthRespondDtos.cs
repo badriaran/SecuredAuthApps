ecafabf726a60b38f799452b74f43336eb304a62
Badri Raj Aran <badriaran10@gmail.com>
2024-04-27 12:24:15 +0545

    intial setup, configure dbcontext , added initial migration, configure jwt authentication, configured identity

---
+++ SecuredAuthApp/SecuredAuthApp/DTOs/AuthRespondDtos.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace SecuredAuthApp.DTOs
+{
+    public class AuthRespondDtos
+    {
+        public string? Token { get; set; }=string.Empty;    
+        public bool IsSuccess { get; set; } 
+        public string? Message { get; set; }
+    }
+}
---
+++ SecuredAuthApp/SecuredAuthApp/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using SecuredAuthApp.Models;
+
+namespace SecuredAuthApp.Data
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\mssqllocaldb;Initial Catalog=SecuredAuthDB;Integrated Security=true");
+        }
+
+    }
+}
